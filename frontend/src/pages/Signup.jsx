d61b90d108be3d1c82c2d6c360c6ba067c0df523
krishnadev7 <krishnadevv12@gmail.com>
2023-04-20 11:24:10 +0530

    upload image on signup form is completed

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -6,11 +6,13 @@ import toast, { Toaster } from 'react-hot-toast';
 
 import {useFormik} from 'formik'
 import * as Yup from 'yup';
+import convertToBase64 from '../utils/convertToBase64';
 
 const Signup = () => {
   const navigate = useNavigate();
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setConfirmPassword] = useState(false);
+  const [uploadedImage,setUploadImage] = useState();
 
   const formik =  useFormik({
     initialValues: {
@@ -27,7 +29,8 @@ const Signup = () => {
       password: Yup.string().min(6, 'Password must be 6 characters long').required('password is required'),
       confirmPassword: Yup.string().oneOf([Yup.ref('password'),null], 'Passwords must match..!').required("Please confirm your password..!")
     }),
-    onSubmit: values => {
+    onSubmit: async values => {
+      values = await Object.assign(values, {profileImage: uploadedImage || ''})
       toast('Successfully Registered...!', {
         icon: '👍',
         style: { backgroundColor: 'green', color: 'white' },
@@ -36,7 +39,7 @@ const Signup = () => {
       setTimeout(() => {
        navigate('/login');
       },4000);
-      
+
       console.log(values);
     }
   })
@@ -51,13 +54,24 @@ const Signup = () => {
     setConfirmPassword(!showConfirmPassword);
   };
 
+  const onUpload = async(e) => {
+    const data = await convertToBase64(e.target.files[0])
+    setUploadImage(data);
+  }
+
   return (
     <div className='p-3 md:p-4'>
       <Toaster position='top-center' reverseOrder={false} />
       <div className='w-full max-w-sm bg-white m-auto flex flex-col items-center p-4'>
         {/* <h1 className='text-center text-2xl font-bold'>SignUp</h1> */}
-        <div className='w-20 overflow-hidden drop-shadow-md shadow-md rounded-full '>
-          <img src={signUp} alt='' className='w-full' />
+        <div className='w-20 h-20 overflow-hidden drop-shadow-md shadow-md rounded-full relative '>
+          <label htmlFor='profileImage' className='cursor-pointer'>
+            <img src={uploadedImage? uploadedImage : signUp} alt='' className='w-full ' />
+            <div className='absolute bottom-0 h-1/3 bg-slate-500 bg-opacity-50 w-full h-full text-center '>
+              <p className='text-sm p-1 text-white'>Upload</p>
+            </div>
+            <input type='file' id='profileImage' accept='image/*' className='hidden' onChange={onUpload}/>
+          </label>
         </div>
 
         <form
